eeab0cc927a9f9d9c3c1c040d0247532df5ab47e
XyrelTenz <xdemocrito1@gmail.com>
2025-11-04 18:06:05 +0800

    Implementing BoardingPage

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -47,6 +47,38 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           OnboardingIndicator(controller: _controller, count: 3),
 
           const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              if (isLastPage) {
+                // navigate to home, login, etc.
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const Placeholder()),
+                );
+              } else {
+                _controller.nextPage(
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.easeInOut,
+                );
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(double.infinity, 50),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(isLastPage ? "Get Started" : "Next"),
+                const SizedBox(width: 8),
+                // 👇 Show different icons based on isLastPage
+                Icon(
+                  isLastPage
+                      ? Icons.arrow_forward_ios_rounded
+                      : Icons.navigate_next_rounded,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
